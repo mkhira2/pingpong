bf0f57f1195766c7b58f8deee6ec56f417d222df
Justin Richards <richards.justind@gmail.com>
2017-03-02 10:21:05 -0600

    replaced modulo row number checking with a css rule

--- js/script.js
+++ js/script.js
@@ -2,24 +2,18 @@ var constructTable = function(apiResponse) {
 	var tableNode = document.querySelector('.tableBody')
 	var htmlString = ''
 	for (var i = 0; i < apiResponse.length; i++) {
-		if(!(i%2)){
-			htmlString += '<tr style="background: #white">'
-		}
-		else
-		{
-			htmlString += '<tr style="background: #eee">'
-		}
-	htmlString += '<td>' + apiResponse[i].name + '</td>'
-	htmlString += '<td>' + apiResponse[i].win_count + '</td>'
-	htmlString += '<td>' + apiResponse[i].loss_count + '</td>'
-	htmlString += '<td>' + apiResponse[i].winning_percentage + '</td></tr>'
+		htmlString += '<tr>'
+		htmlString += 	'<td>' + apiResponse[i].name + '</td>'
+		htmlString += 	'<td>' + apiResponse[i].win_count + '</td>'
+		htmlString += 	'<td>' + apiResponse[i].loss_count + '</td>'
+		htmlString += 	'<td>' + apiResponse[i].winning_percentage + '</td>'
+		htmlString += '</tr>'
 	}
 	tableNode.innerHTML = htmlString
 }
 setInterval(function() {
-var promise = $.getJSON('https://ironpong.herokuapp.com/home/index.json')
-
-promise.then(constructTable)
+	var promise = $.getJSON('https://ironpong.herokuapp.com/home/index.json')
+	promise.then(constructTable)
 }, 1000)
 
 
